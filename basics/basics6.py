57e67f10fc3849e9e7110262f4d40ad5e0bc4230
Alexey Ignatenko <a.ignatenko1988@gmail.com>
2017-09-03 20:58:09 +0300

    basics/6th task

---
+++ basics/basics6.py
@@ -0,0 +1,16 @@
+#swaps symbols in the string: first symbol becomes second, second becomes third, last becomes first
+def caesar_cipher(string, number):
+    list = []
+    ciphered = []
+    for i in range(len(string)):
+        if i+number <= len(string)-1:
+            list.append(i+number)
+            ciphered += string[list[i]]
+        else:
+            list.append((i + number)-len(string))
+            ciphered += string[list[i]]
+    print list
+    print ''.join(ciphered)
+
+
+caesar_cipher('terry', -1)
